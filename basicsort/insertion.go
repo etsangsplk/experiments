0279f07ffe226b3efc9d7c67bdb046e2236ab5d7
etsang <etsang@loaner-mac-a5c63.sv.splunk.com>
2019-03-24 15:43:08 -0700

    insertion and selection sort

--- basicsort/insertion.go
+++ basicsort/insertion.go
@@ -17,27 +17,38 @@ func main() {
 
 		for k, v := range b {
 			if v != a[k] {
-				fmt.Printf("different at :%d", k)
+				fmt.Printf("different at :%d \n", k)
 			}
 		}
 		fmt.Printf("a: %v \nb: %v\n", a, b)
 	}
 }
 
-// Maintain a sorted and "unknown" sections.
-// Sorted section started at 0
-// Try to find the smallest element from the unknown sections
-// and add to the "sorted" section.
+// Partition list into sorted and unsorted sections.
+// Sorted section starts as 1 single element.
+// Empty list is ignored
+// Invariant is S is sorted.
+// Just grab an element from Unsorted list
+// * comparing this element to sorted list starting from last element of sorted list
+// * swap the previous element with current postion to make space
+// * until the postion where the element infront is smaller
+// Repeat until unsorted list is of 1 element
 func insertionSort(a []int) {
 	n := len(a)
-	i := 0
+
+	// start at 1, empty list of size 0.
+	// for-loop will not be executed.
+	i := 1
 	for i < n {
-		j := i + 1
-		for j < n {
-			if a[i] > a[j] {
-				helper.Swap(&a[i], &a[j])
+		j := i
+		// Expand the sorted list boundary by 1, including 1 element of unsorted list
+		// Starting at end of sorted list.
+		for j > 0 {
+			// Compare myself against previous element
+			if a[j] < a[j-1] {
+				helper.Swap(&a[j], &a[j-1])
 			}
-			j++
+			j-- //move point to element
 		}
 		i++
 	}
--- basicsort/selection.go
+++ basicsort/selection.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"fmt"
+	"sort"
+
+	"./helper"
+)
+
+func main() {
+	for i := 0; i < 10; i++ {
+		a := helper.GenerateSlice(i)
+		b := make([]int, len(a))
+		copy(b, a) // copy to b from a
+		selectionSort(a)
+		sort.Ints(b)
+
+		for k, v := range b {
+			if v != a[k] {
+				fmt.Printf("different at :%d", k)
+			}
+		}
+		fmt.Printf("a: %v \nb: %v\n", a, b)
+	}
+}
+
+// Maintain a sorted and "unknown" sections.
+// Sorted section started at 0.
+// Invariant S is sorted.
+// Find the smallest element from the unknown sections
+// and add to the "sorted" section at the end.
+// Repeat until there is 1 left in unsorted list.
+func selectionSort(a []int) {
+	n := len(a)
+	i := 0 // i: sorted section
+	for i < n {
+		j := i + 1 // j: unsorted section
+		for j < n {
+			if a[i] > a[j] {
+				helper.Swap(&a[i], &a[j]) // swap or append to end of sorted section
+			}
+			j++
+		}
+		i++
+	}
+
+}
