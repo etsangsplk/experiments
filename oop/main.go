a626007ba719c1e3eeb5465a81546e1771cce12c
edward tsang <etsang@splunk.com>
2019-09-29 14:03:07 -0700

    Add nanoyous strucT

--- oop/main.go
+++ oop/main.go
@@ -12,4 +12,10 @@ func main() {
 		fmt.Printf("k :%v v: %v\n ", k, v.printMe())
 	}
 
+
+	fmt.Printf("annonymout struct: %v \n", struct {
+		   age int
+		   name string
+		}{ age: 10,  // have to repeat definition of anonymout struct if more than 1 fields
+		   name: "wawa",})
 }
