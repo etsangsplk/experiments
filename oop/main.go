371148b34b1817cff4e17dc657ea96cf59584061
edward tsang <etsang@splunk.com>
2019-09-29 14:45:43 -0700

    checkn in for now

--- oop/main.go
+++ oop/main.go
@@ -29,4 +29,29 @@ func main() {
 
 	fmt.Printf("x is even math floor is %v \n", math.Floor(float64(4/2)))
 
+
+	//Ceiling operator
+
+	fmt.Printf("x is odd math ceil is %v \n", math.Ceil(float64(3/2)))
+
+	fmt.Printf("x is even math ceil is %v \n", math.Ceil(float64(4/2)))
+
+	// normal integerter division also floored the result
+	fmt.Printf("x is odd nothing is %v\n", (3/2))
+
+	fmt.Printf("x is even nothing is %v \n", (4/2))
+
+		// normal integerter division also floored the result
+	fmt.Printf("x is odd nothing is float64 all to gt precision  %v\n", float64(3)/float64(2))
+
+	fmt.Printf("x is even nothing is float64 all tog et preiceison %v \n", float64(4)/float64(2))
+
+	fmt.Printf("x is odd nothing is float64 all then math Ceil %v\n", math.Ceil(float64(3)/float64(2)))
+
+	fmt.Printf("x is even nothing is float64 all then math Ceil %v \n", math.Ceil(float64(4)/float64(2)))
+
+	fmt.Printf("x is odd nothing is float64 all then math Floor %v\n", math.Floor(float64(3)/float64(2)))
+
+	fmt.Printf("x is even nothing is float64 all then math Floor %v \n", math.Floor(float64(4)/float64(2)))
+
 }
