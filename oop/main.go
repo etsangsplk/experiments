7b0f5bcccdfe63dea1cd6d8c7bb7e22e712ddde7
edward tsang <etsang@splunk.com>
2019-09-29 14:53:58 -0700

    checkn in for now

--- oop/main.go
+++ oop/main.go
@@ -26,32 +26,29 @@ func main() {
 
 	//Floor operator
 	fmt.Printf("x is odd math floor is %v  \n", math.Floor(float64(3/2)))
-
 	fmt.Printf("x is even math floor is %v \n", math.Floor(float64(4/2)))
 
 
 	//Ceiling operator
-
+	// result already rounded down by integer division
 	fmt.Printf("x is odd math ceil is %v \n", math.Ceil(float64(3/2)))
-
 	fmt.Printf("x is even math ceil is %v \n", math.Ceil(float64(4/2)))
 
 	// normal integerter division also floored the result
 	fmt.Printf("x is odd nothing is %v\n", (3/2))
-
 	fmt.Printf("x is even nothing is %v \n", (4/2))
 
-		// normal integerter division also floored the result
-	fmt.Printf("x is odd nothing is float64 all to gt precision  %v\n", float64(3)/float64(2))
-
-	fmt.Printf("x is even nothing is float64 all tog et preiceison %v \n", float64(4)/float64(2))
+	// normal integerter division also floored the result
+	// float should not round down 
+	fmt.Printf("x is odd nothing is float64 all to get precision  %v\n", float64(3)/float64(2))
+	fmt.Printf("x is even nothing is float64 all to get preiceison %v \n", float64(4)/float64(2))
 
 	fmt.Printf("x is odd nothing is float64 all then math Ceil %v\n", math.Ceil(float64(3)/float64(2)))
-
 	fmt.Printf("x is even nothing is float64 all then math Ceil %v \n", math.Ceil(float64(4)/float64(2)))
 
 	fmt.Printf("x is odd nothing is float64 all then math Floor %v\n", math.Floor(float64(3)/float64(2)))
-
 	fmt.Printf("x is even nothing is float64 all then math Floor %v \n", math.Floor(float64(4)/float64(2)))
+
+
 
 }
