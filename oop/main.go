0c2e131716ef337d8c51d3d611c62d0927c09a9e
edward tsang <etsang@splunk.com>
2019-09-29 14:17:16 -0700

    some random experienemtns

--- oop/main.go
+++ oop/main.go
@@ -1,21 +1,26 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+)
 
 func main() {
 	c := NewCar()
 	b := NewBus()
-	// program against the interface 
+	// program against the interface
 	vl := []VehiclePrinter{c, b}
 
 	for k, v := range vl {
 		fmt.Printf("k :%v v: %v\n ", k, v.printMe())
 	}
 
-
 	fmt.Printf("annonymout struct: %v \n", struct {
-		   age int
-		   name string
-		}{ age: 10,  // have to repeat definition of anonymout struct if more than 1 fields
-		   name: "wawa",})
+		age  int
+		name string
+	}{age: 10, // have to repeat definition of anonymout struct if more than 1 fields
+		name: "wawa"})
+
+	fmt.Println(func() string { return "ddmfkdmfdmg" }())
+	fmt.Println(func(x string) string { return fmt.Sprintf("yyyyy%s", x) }("ddmfkdmfdmg"))
+
 }
--- oop/oop.go
+++ oop/oop.go
@@ -1,15 +1,16 @@
 package main
 
-import ( "fmt")
+import (
+	"fmt"
+)
 
 type VehiclePrinter interface {
 	printMe() string
 }
 
-
 type Vehicle struct {
 	LicensePlate string
-	Vehicletype string
+	Vehicletype  string
 }
 
 func (v *Vehicle) PLicensePlate() string {
@@ -31,20 +32,19 @@ type Car struct {
 	comment string
 }
 
-func NewCar() *Car{
+func NewCar() *Car {
 	return &Car{
 		// embedded annoymous struct available via type only
-		Vehicle: Vehicle{ LicensePlate: "1234",
-						  Vehicletype: "car",},
-		FourWheeler: FourWheeler{ wheels: 4,},
-		comment: "i am a car",
+		Vehicle: Vehicle{LicensePlate: "1234",
+			Vehicletype: "car"},
+		FourWheeler: FourWheeler{wheels: 4},
+		comment:     "i am a car",
 	}
 }
 
 func (c *Car) printMe() string {
 	return fmt.Sprintf("licensePlate: %v car details: %v\n", c.PLicensePlate(), c.String())
 }
-	
 
 type Bus struct {
 	Vehicle
@@ -52,15 +52,14 @@ type Bus struct {
 	comment string
 }
 
-func NewBus() *Bus{
-	return &Bus {
-		Vehicle:  Vehicle{ LicensePlate: "5678",
-		 		   Vehicletype: "bus",},
-		FourWheeler:  FourWheeler{wheels: 2,},
-		comment: "i am a bus",
+func NewBus() *Bus {
+	return &Bus{
+		Vehicle: Vehicle{LicensePlate: "5678",
+			Vehicletype: "bus"},
+		FourWheeler: FourWheeler{wheels: 2},
+		comment:     "i am a bus",
 	}
 }
-
 
 func (b *Bus) printMe() string {
 	return fmt.Sprintf("bus details : %v\n", b)
