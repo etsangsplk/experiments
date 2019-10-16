62e54dd56793102aecf64fb49c199297ab5ea839
Edward Tsang <etsang@splunk.com>
2019-10-16 16:27:54 -0700

    use tablulization

--- recurssion/tribonacci.go
+++ recurssion/tribonacci.go
@@ -2,14 +2,37 @@ package main
 
 import "fmt"
 
+var result = make([]int, 36)
+
 func tribonacci(n int) int {
 	switch n {
 	case 0:
-		return 0
-	case 1, 2:
-		return 1
+		result[0] = 0
+		return result[0]
+	case 1:
+		result[1] = 1
+		return result[1]
+	case 2:
+		result[2] = 1
+		return result[2]
 	default:
-		return tribonacci(n-3) + tribonacci(n-2) + tribonacci(n-1)
+		var n3, n2, n1 int
+		if 0 != result[n-3] {
+			n3 = result[n-3]
+		} else {
+			n3 = tribonacci(n - 3)
+		}
+		if 0 != result[n-2] {
+			n2 = result[n-2]
+		} else {
+			n2 = tribonacci(n - 2)
+		}
+		if 0 != result[n-1] {
+			n1 = result[n-1]
+		} else {
+			n1 = tribonacci(n - 1)
+		}
+		return n3 + n2 + n1
 	}
 }
 
@@ -18,4 +41,5 @@ func main() {
 	fmt.Println(tribonacci(n))
 	fmt.Println(tribonacci(0))
 	fmt.Println(tribonacci(2))
+	fmt.Println(tribonacci(36))
 }
