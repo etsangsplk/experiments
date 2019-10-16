0f5841d845387035cdcc11f31da0f050d7ce9749
Edward Tsang <etsang@splunk.com>
2019-10-16 16:32:56 -0700

    should be 37 as upper bound

--- recurssion/tribonacci.go
+++ recurssion/tribonacci.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-var result = make([]int, 36)
+var result = make([]int, 37)
 
 func tribonacci(n int) int {
 	switch n {
@@ -21,16 +21,19 @@ func tribonacci(n int) int {
 			n3 = result[n-3]
 		} else {
 			n3 = tribonacci(n - 3)
+			result[n-3] = n3
 		}
 		if 0 != result[n-2] {
 			n2 = result[n-2]
 		} else {
 			n2 = tribonacci(n - 2)
+			result[n-2] = n2
 		}
 		if 0 != result[n-1] {
 			n1 = result[n-1]
 		} else {
 			n1 = tribonacci(n - 1)
+			result[n-1] = n1
 		}
 		return n3 + n2 + n1
 	}
