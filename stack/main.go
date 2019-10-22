089c6bdce87a0c6cba9b9db171971fbb245dce82
Edward Tsang <etsang@splunk.com>
2019-10-22 14:33:02 -0700

    simple implemention of stack

--- stack/main.go
+++ stack/main.go
@@ -1,1 +1,23 @@
 package main
+
+import "fmt"
+
+type bin int
+
+func (b bin) String() string {
+	return fmt.Sprintf("%b", b)
+}
+
+func main() {
+	s := New(0)
+	fmt.Printf("%v\n", s.Pop())
+	fmt.Printf("%v\n", s.Size())
+	fmt.Printf("%v\n", s.IsFull())
+	fmt.Printf("%v\n", s.IsEmpty())
+	s = New(1)
+	s.Push(bin(1))
+	fmt.Printf("%v\n", s.Pop())
+	fmt.Printf("%v\n", s.Size())
+	fmt.Printf("%v\n", s.IsFull())
+	fmt.Printf("%v\n", s.IsEmpty())
+}
--- stack/stack.go
+++ stack/stack.go
@@ -1,1 +1,59 @@
 package main
+
+import "fmt"
+
+type Item interface {
+	fmt.Stringer
+}
+
+type Stack interface {
+	Pop() Item
+	Push(Item)
+	IsFull() bool
+	IsEmpty() bool
+	Size() int
+}
+
+type S struct {
+	Stack
+	Items    []Item
+	size     int
+	capacity int
+}
+
+func New(s int) *S {
+	return &S{
+		Items:    make([]Item, s),
+		capacity: s,
+	}
+}
+
+func (s *S) Push(i Item) {
+	if s.IsFull() {
+		return
+	}
+	s.Items[s.size] = i
+	s.size++
+}
+
+func (s *S) Pop() Item {
+	var x Item
+	if s.IsEmpty() {
+		return x
+	}
+	x = s.Items[s.size-1]
+	s.Items = s.Items[:(s.size - 1)]
+	s.size--
+	return x
+}
+
+func (s *S) IsEmpty() bool {
+	return s.Size() == 0
+}
+
+func (s *S) IsFull() bool {
+	return s.Size() == s.capacity
+}
+func (s *S) Size() int {
+	return s.size
+}
