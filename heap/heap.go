deb026c0079858e699a477da3f79d0e4c09f9ab9
Edward Tsang <etsang@splunk.com>
2019-10-27 15:44:03 -0700

    heap:

--- heap/heap.go
+++ heap/heap.go
@@ -11,3 +11,24 @@ type Heap interface {
 	Swap(x, y *Item)
 	Heapify()
 }
+
+type H1 struct {
+	Heap
+	Items []Item
+}
+
+func (h *H1) Push(x Item) {
+
+}
+
+func (h *H1) Pop() Item {
+
+}
+
+func (h *H1) Swap(x, y *Item) {
+
+}
+
+func (h *H1) Heapify() {
+
+}
